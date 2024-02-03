78bc393c3a8f8cb853631df52ccedec99c43f2da
eodaneze <ezealiokechukwu1999@gmail.com>
2024-02-03 12:59:49 -0800

    user registration

--- customer/controller/authController.js
+++ customer/controller/authController.js
@@ -1,5 +1,21 @@
+const Customer = require("../models/Customer");
 
 const register = async(req, res) => {
-   res.send("Hello from custermer router");
+    try{
+         const {username, fullname, email, password} = req.body;
+         const exisitingUser = await Customer.findOne({$or: [{username}, {email}]});
+         if(exisitingUser) return res.status(400).json({message: "Username or email already exists"});
+
+         const newCustomer = new Customer({
+            username,
+            fullname,
+            email,
+            password
+         });
+         await newCustomer.save();
+         res.status(200).json({message: "Your registration was successful", newCustomer})
+    }catch(err){
+        console.log(err)
+    }
 }
 module.exports = {register}

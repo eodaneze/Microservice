fa72e8d0fc05a04019d3bad298dbd5e5bc8570f5
eodaneze <ezealiokechukwu1999@gmail.com>
2024-02-03 13:45:46 -0800

    user login after authentication and verification

--- customer/controller/authController.js
+++ customer/controller/authController.js
@@ -7,23 +7,24 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 const verification_secret = process.env.verification_secret_key;
+const secret_key = process.env.JWT_SECRET;
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      customer: process.env.EMAIL_USER,
+      customer: process.env.EMAIL_customer,
       pass: process.env.EMAIL_PASS,
     },
   });
 const register = async(req, res) => {
     try{
-         const {username, fullname, email, password} = req.body;
+         const {customername, fullname, email, password} = req.body;
          if (!isValidEmail(email)) return res.status(400).json({ message: 'Invalid email format' });
-         const exisitingUser = await Customer.findOne({$or: [{username}, {email}]});
-         if(exisitingUser) return res.status(400).json({message: "Username or email already exists"});
+         const exisitingcustomer = await Customer.findOne({$or: [{customername}, {email}]});
+         if(exisitingcustomer) return res.status(400).json({message: "customername or email already exists"});
          const hashedPassword = await bcrypt.hash(password, 10);
          const verificationToken = jwt.sign({ fullname, email }, verification_secret);
          const newCustomer = new Customer({
-            username,
+            customername,
             fullname,
             email,
             password: hashedPassword,
@@ -32,7 +33,7 @@ const register = async(req, res) => {
          await newCustomer.save();
          const verificationLink = `http://localhost:5000/verify/token=${verificationToken}`;
          const mailOptions = {
-            from: process.env.EMAIL_USER,
+            from: process.env.EMAIL_customer,
             to: email,
             subject: 'Welcome to Nodemicroservice',
             html: `Dear ${fullname}, Thank you for registering at nodemicroservice, Click <a href="${verificationLink}">here</a> to verify your email.`,
@@ -44,7 +45,7 @@ const register = async(req, res) => {
                res.status(400).json({ message: 'error registering customer'});
             }else{
                            
-               res.status(200).json({ message: `User registered successfully. Verification email sent to ${email}`});
+               res.status(200).json({ message: `customer registered successfully. Verification email sent to ${email}`});
             }
          });
         
@@ -75,4 +76,33 @@ const verifyEmail = async (req, res) => {
       res.status(500).json({ message: 'Internal server error' });
     }
   }
-module.exports = {register, verifyEmail}
+
+
+  const authLogin = async(req, res) => {
+    const{email, password} = req.body;
+    if(!email || !password) return res.status(400).json({error: "Please provide email and password"});
+    try{
+        //  find the customer by email in the database
+         const customer = await Customer.findOne({email});
+      
+        //  check if the customer exist
+        if(!customer) res.status(400).json({error: 'customer with this record does not exist in the database'});
+    //    compare the password with the strored hashed password
+        const passwordMarch = await bcrypt.compare(password, customer.password);
+        if(!passwordMarch) res.status(401).json({error: 'Incorrect password'});
+        // check if the customer have been verified
+        if(!customer.isVerified) return res.status(401).json({message: `You have not verified your account please check your email ${email} to verify your account`})
+       // Use destructuring with the spread operator to exclude password from the response
+       const {password: _, ...others} = customer.toObject();
+         // use a jwt to authenticate a customer
+         const accessToken = jwt.sign({
+            id: customer._id, 
+            email: customer.email, role: customer.role}, secret_key, {
+            expiresIn: '6h',
+          });
+        res.status(200).json({message: "Login successful", others, accessToken})
+    }catch(err){
+       console.log(err)
+    }
+}
+module.exports = {register, verifyEmail, authLogin}
--- customer/route/auth.js
+++ customer/route/auth.js
@@ -1,8 +1,9 @@
-const { register, verifyEmail } = require('../controller/authController');
+const { register, verifyEmail, authLogin } = require('../controller/authController');
 
 const router = require('express').Router();
 
 router.post("/register", register)
 router.post('/verify/:token',  verifyEmail);
+router.post('/login',  authLogin);
 
 module.exports = router

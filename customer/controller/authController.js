59b2911cf97f32b41721e53d1d53b9eabc5a4600
eodaneze <ezealiokechukwu1999@gmail.com>
2024-02-03 13:07:31 -0800

    hashing customers password

--- customer/controller/authController.js
+++ customer/controller/authController.js
@@ -1,16 +1,17 @@
 const Customer = require("../models/Customer");
+const bcrypt = require('bcrypt');
 
 const register = async(req, res) => {
     try{
          const {username, fullname, email, password} = req.body;
          const exisitingUser = await Customer.findOne({$or: [{username}, {email}]});
          if(exisitingUser) return res.status(400).json({message: "Username or email already exists"});
-
+         const hashedPassword = await bcrypt.hash(password, 10);
          const newCustomer = new Customer({
             username,
             fullname,
             email,
-            password
+            password: hashedPassword
          });
          await newCustomer.save();
          res.status(200).json({message: "Your registration was successful", newCustomer})
